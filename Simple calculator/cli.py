c793157118ea639b89e73aad1f0f874f5f3c67d2
Cladvyr <167028773+Cladvyr@users.noreply.github.com>
2026-07-10 21:57:48 +0200

    Debugged code, tried to optimise error handling when setting zero as second number, tried different solutions that weren't logic in the end or couldn't work, changed number type from integer to float

--- Simple calculator/cli.py
+++ Simple calculator/cli.py
@@ -1,10 +1,10 @@
 from main_logic import add, substract as sub, multiply as mul, divide as div
 
-first_number = int(input("Enter the first number: "))
-if first_number != int:
+first_number = float(input("Enter the first number: "))
+if first_number != float or first_number != int:
     print("Please enter a valid number.")
 
-second_number = int(input("Enter the second number: "))
+second_number = float(input("Enter the second number: "))
 if second_number != int:
     print("Please enter a valid number.")
 
@@ -21,12 +21,11 @@ elif operation == "mul":
 elif operation == "div":
     if second_number == 0:
         print("Cannot divide by zero.")
-        second_number = int(input("Choose a different second number: "))
-        if second_number == 0:
+        second_number = float(input("Choose a different second number: "))
+        while second_number == 0:
             print("Invalid input. Cannot divide by zero.")
-        else:
-            result = div(first_number, second_number)
-    else:
+            second_number = float(input("Choose a different second number: "))
+            if second_number == 0 or second_number != "":
+                print("Please enter a valid number.")
         result = div(first_number, second_number)
-else:
-    print(f"Result: {result}")
+print(f"Result: {result}")
