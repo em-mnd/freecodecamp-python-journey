4bd87b6e2d6885183ded7170dc7735d13f9e417f
Riri <167028773+em-mnd@users.noreply.github.com>
2026-07-13 20:01:42 +0200

    Fixed indentation errors and fixed typos

--- Student Life Manager (SLM)/main.py
+++ Student Life Manager (SLM)/main.py
@@ -1,8 +1,8 @@
 def menu():
-# Display the main menu options to the user
+    # Display the main menu options to the user
 
-# Print menu options and ask for input
-# Make a choice variable to store user choice, depending on choice another menu might be displayed
-# depending on the feature selected or the suer might want to exit
-
-# here there is no logic for other features, it's a gateway for them.
+    # Print menu options and ask for input
+    # Make a choice variable to store user choice, depending on choice another menu might be displayed
+    # depending on the feature selected or the user might want to exit
+    # here there is no logic for other features, it's a gateway for them
+    pass
